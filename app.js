41a7628bf36ea0c0531640f1cf218fc41dcd28a4
Syed Misam Hasnain Rivi <misamhasnain45@gmail.com>
2026-01-29 21:57:03 +0500

    update digital clock code

--- app.js
+++ app.js
@@ -19,9 +19,10 @@
                 h = h - 12;
             }
             
-            h = h < 10 ? "0" + h : h;
-            m = m < 10 ? "0" + m : m;
-            s = s < 10 ? "0" + s : s;
+            if (h < 10) h = "0" + h;
+            if (m < 10) m = "0" + m;
+            if (s < 10) s = "0" + s;
+
 
             document.getElementById('hours').innerText = h;
             document.getElementById('min').innerText = m;
